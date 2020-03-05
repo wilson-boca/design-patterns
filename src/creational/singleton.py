4a178705f1464e35c2f0681855c2503c0ae89970
Wilson <wilson.boca@gmail.com>
2020-03-05 12:47:08 -0300

    Implementing singleton.

--- src/creational/singleton.py
+++ src/creational/singleton.py
@@ -1,3 +1,13 @@
-
 class Singleton(object):
-    pass
+    # overwriting __new__ method, this is how we ensure that it returns always the same object
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(Singleton, cls).__new__(cls)
+        return cls.instance
+
+
+s0 = Singleton()
+s1 = Singleton()
+print(s0)
+print(s1)
+print(s0 == s1)
